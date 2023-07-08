7e1bc10eb9cc3831a0654b3fa6b0c17949a25c87
guemax <github-mg@guenther.cloud>
2023-07-08 11:42:37 +0200

    Update character script

--- src/scripts/character.gd
+++ src/scripts/character.gd
@@ -1,28 +1,39 @@
 extends CharacterBody2D
 
+const UP_DIRECTION := Vector2.UP
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed := 600.0
+@export var jump_strength := 1500.0
+@export var maximum_number_of_jumps := 2
+@export var double_jump_strength := 1200.0
+@export var gravity := 4500.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _jumps_made := 0
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var horizontal_direction = (
+		Input.get_action_strength("move_right")
+		- Input.get_action_strength("move_left")
+	)
+	
+	velocity.x = horizontal_direction * speed
+	velocity.y += gravity * delta
+	
+	var is_falling := velocity.y > 0.0 and not is_on_floor()
+	var is_jumping := Input.is_action_just_released("jump") and is_on_floor()
+	var is_double_jumping := Input.is_action_just_released("jump") and is_falling
+	var is_jump_cancelled := Input.is_action_just_released("jump") and velocity.y > 0.0
+	var is_idling := is_on_floor() and is_zero_approx(velocity.x)
+	var is_running := is_on_floor() and not is_zero_approx(velocity.x)
+	
+	if is_jumping or is_double_jumping:
+		_jumps_made += 1
+		if _jumps_made <= maximum_number_of_jumps:
+			velocity.y = -jump_strength
+	elif is_jump_cancelled:
+		velocity.y = 0.0
+	elif is_idling or is_running:
+		_jumps_made = 0
+	
 	move_and_slide()
